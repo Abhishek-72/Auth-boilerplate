6dd3df91f461860faea57005a5fea5569bd87ef0
Abhishek Tripathi <Abhishek-72>
2024-11-28 13:47:27 +0530

    connected to database

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,15 @@
 const express = require("express");
 require("dotenv").config();
+const mongoose = require("mongoose");
 const app = express();
+
+// DB connection
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose
+  .connect(MONGO_URI)
+  .then(() => console.log("Mongo Db is connected"))
+  .catch((e) => console.log(e));
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
